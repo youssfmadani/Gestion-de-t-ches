dad77c5c018f848c34b1e8651d27e628161238a1
youssfmadani <youssfelmadani@gmail.com>
2024-10-29 10:22:25 +0100

    Implement Task and Date structures

--- apptache.c
+++ apptache.c
@@ -3,6 +3,20 @@
 
 #define MAX 30
 
+typedef struct {
+    char title[100];
+    char description[256];
+    Date due_date;
+    char priority[10];
+    int status;
+} Task;
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+} Date;
+
 Task tasks[MAX_TASKS];
 int task_count = 0;
 
